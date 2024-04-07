5a5f71c6dca3284b2d186e567f752945d83217f1
SushiulkumarDev <sushilkumardeveloper@gmail.com>
2024-04-07 14:32:03 +0530

    Develop a topnav and Sidenav bar and login and signup button

---
+++ script.js
@@ -0,0 +1,55 @@
+function openSidebar() {
+    document.getElementById("mySidebar").style.width = "250px";
+  }
+  
+  function closeSidebar() {
+    document.getElementById("mySidebar").style.width = "0";
+  }
+  
+  function toggleForm(formId) {
+    var form = document.getElementById(formId);
+    if (form.style.display === "block") {
+      form.style.display = "none";
+    } else {
+      form.style.display = "block";
+    }
+  }
+  
+  function closeForm(formId) {
+    document.getElementById(formId).style.display = "none";
+  }
+  
+  // Toggle responsive top navigation
+  function toggleResponsiveNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  function openLoginForm() {
+    document.getElementById("loginForm").style.display = "block";
+  }
+  
+  function closeLoginForm() {
+    document.getElementById("loginForm").style.display = "none";
+  }
+  
+  function openSignupForm() {
+    document.getElementById("signupForm").style.display = "block";
+  }
+  
+  function closeSignupForm() {
+    document.getElementById("signupForm").style.display = "none";
+  }
+  
+  function openSidebar() {
+    document.getElementById("mySidebar").style.width = "250px";
+  }
+  
+  function closeSidebar() {
+    document.getElementById("mySidebar").style.width = "0";
+  }
+  
+  
